4fc6454d5d66a9058a553d95dec95001b0da952c
Santiago de Pablo de Castro <santiagodepablodecastro@MacBook-Air-de-Santiago.local>
2026-05-19 10:58:40 +0100

    Fix Goldeen evolution and update missing evolutions (Porygon, Eevee, Kirlia, Nincada)

--- pokemon-screen/src/constants/pokemon.ts
+++ pokemon-screen/src/constants/pokemon.ts
@@ -5,6 +5,11 @@ export const POKE_IDS: { [key: string]: number } = {
   chikorita: 152, bayleef: 153, meganium: 154, cyndaquil: 155, quilava: 156, typhlosion: 157, totodile: 158, croconaw: 159, feraligatr: 160, sentret: 161, furret: 162, hoothoot: 163, noctowl: 164, ledyba: 165, ledian: 166, spinarak: 167, ariados: 168, crobat: 169, chinchou: 170, lanturn: 171, pichu: 172, cleffa: 173, igglybuff: 174, togepi: 175, togetic: 176, natu: 177, xatu: 178, mareep: 179, flaaffy: 180, ampharos: 181, bellossom: 182, marill: 183, azumarill: 184, sudowoodo: 185, politoed: 186, hoppip: 187, skiploom: 188, jumpluff: 189, aipom: 190, sunkern: 191, sunflora: 192, yanma: 193, wooper: 194, quagsire: 195, espeon: 196, umbreon: 197, murkrow: 198, slowking: 199, misdreavus: 200, unown: 201, wobbuffet: 202, girafarig: 203, pineco: 204, forretress: 205, dunsparce: 206, gligar: 207, steelix: 208, snubbull: 209, granbull: 210, qwilfish: 211, scizor: 212, shuckle: 213, heracross: 214, sneasel: 215, teddiursa: 216, ursaring: 217, slugma: 218, magcargo: 219, swinub: 220, piloswine: 221, corsola: 222, remoraid: 223, octillery: 224, delibird: 225, mantine: 226, skarmory: 227, houndour: 228, houndoom: 229, kingdra: 230, phanpy: 231, donphan: 232, porygon2: 233, stantler: 234, smeargle: 235, tyrogue: 236, hitmontop: 237, smoochum: 238, elekid: 239, magby: 240, miltank: 241, blissey: 242, raikou: 243, entei: 244, suicune: 245, larvitar: 246, pupitar: 247, tyranitar: 248, lugia: 249, ho_oh: 250, celebi: 251,
   // Gen 3 (Hoenn)
   treecko: 252, grovyle: 253, sceptile: 254, torchic: 255, combusken: 256, blaziken: 257, mudkip: 258, marshtomp: 259, swampert: 260, poochyena: 261, mightyena: 262, zigzagoon: 263, linoone: 264, wurmple: 265, silcoon: 266, beautifly: 267, cascoon: 268, dustox: 269, lotad: 270, lombre: 271, ludicolo: 272, seedot: 273, nuzleaf: 274, shiftry: 275, taillow: 276, swellow: 277, wingull: 278, pelipper: 279, ralts: 280, kirlia: 281, gardevoir: 282, surskit: 283, masquerain: 284, shroomish: 285, breloom: 286, slakoth: 287, vigoroth: 288, slaking: 289, nincada: 290, ninjask: 291, shedinja: 292, whismur: 293, loudred: 294, exploud: 295, makuhita: 296, hariyama: 297, azurill: 298, nosepass: 299, skitty: 300, delcatty: 301, sableye: 302, mawile: 303, aron: 304, lairon: 305, aggron: 306, meditite: 307, medicham: 308, electrike: 309, manectric: 310, plusle: 311, minun: 312, volbeat: 313, illumise: 314, roselia: 315, gulpin: 316, swalot: 317, carvanha: 318, sharpedo: 319, wailmer: 320, wailord: 321, numel: 322, camerupt: 323, torkoal: 324, spoink: 325, grumpig: 326, spinda: 327, trapinch: 328, vibrava: 329, flygon: 330, cacnea: 331, cacturne: 332, swablu: 333, altaria: 334, zangoose: 335, seviper: 336, lunatone: 337, solrock: 338, barboach: 339, whiscash: 340, corphish: 341, crawdaunt: 342, baltoy: 343, claydol: 344, lileep: 345, cradily: 346, anorith: 347, armaldo: 348, feebas: 349, milotic: 350, castform: 351, kecleon: 352, shuppet: 353, banette: 354, duskull: 355, dusclops: 356, tropius: 357, chimecho: 358, absol: 359, wynaut: 360, snorunt: 361, glalie: 362, spheal: 363, sealeo: 364, walrein: 365, clamperl: 366, huntail: 367, gorebyss: 368, relicanth: 369, luvdisc: 370, bagon: 371, shelgon: 372, salamence: 373, beldum: 374, metang: 375, metagross: 376, regirock: 377, regice: 378, registeel: 379, latias: 380, latios: 381, kyogre: 382, groudon: 383, rayquaza: 384, jirachi: 385, deoxys: 386,
+  // Gen 4 (Sinnoh)
+  turtwig: 387, grotle: 388, torterra: 389, chimchar: 390, monferno: 391, infernape: 392, piplup: 393, prinplup: 394, empoleon: 395, starly: 396, staravia: 397, staraptor: 398, bidoof: 399, bibarel: 400, kricketot: 401, kricketune: 402, shinx: 403, luxio: 404, luxray: 405, budew: 406, roserade: 407, cranidos: 408, rampardos: 409, shieldon: 410, bastiodon: 411, burmy: 412, wormadam: 413, mothim: 414, combee: 415, vespiquen: 416, pachirisu: 417, buizel: 418, floatzel: 419, cherubi: 420, cherrim: 421, shellos: 422, gastrodon: 423, ambipom: 424, drifloon: 425, drifblim: 426, buneary: 427, lopunny: 428, mismagius: 429, honchkrow: 430, glameow: 431, purugly: 432, chingling: 433, stunky: 434, skuntank: 435, bronzor: 436, bronzong: 437, bonsly: 438, mime_jr: 439, happiny: 440, chatot: 441, spiritomb: 442, gible: 443, gabite: 444, garchomp: 445, munchlax: 446, riolu: 447, lucario: 448, hippopotas: 449, hippowdon: 450, skorupi: 451, drapion: 452, croagunk: 453, toxicroak: 454, carnivine: 455, finneon: 456, lumineon: 457, mantyke: 458, snover: 459, abomasnow: 460, weavile: 461, magnezone: 462, lickilicky: 463, rhyperior: 464, tangrowth: 465, electivire: 466, magmortar: 467, togekiss: 468, yanmega: 469, leafeon: 470, glaceon: 471, gliscor: 472, mamoswine: 473, porygon_z: 474, gallade: 475, probopass: 476, dusknoir: 477, froslass: 478, rotom: 479, uxie: 480, mesprit: 481, azelf: 482, dialga: 483, palkia: 484, heatran: 485, regigigas: 486, giratina: 487, cresselia: 488, phione: 489, manaphy: 490, darkrai: 491, shaymin: 492, arceus: 493,
+  // Gen 5 (Unova)
+  victini: 494, snivy: 495, servine: 496, serperior: 497, tepig: 498, pignite: 499, emboar: 500, oshawott: 501, dewott: 502, samurott: 503, patrat: 504, watchog: 505, lillipup: 506, herdier: 507, stoutland: 508, purrloin: 509, liepard: 510, pansage: 511, simisage: 512, pansear: 513, simisear: 514, panpour: 515, simipour: 516, munna: 517, musharna: 518, pidove: 519, tranquill: 520, unfezant: 521, blitzle: 522, zebstrika: 523, roggenrola: 524, boldore: 525, gigalith: 526, woobat: 527, swoobat: 528, drilbur: 529, excadrill: 530, audino: 531, timburr: 532, gurdurr: 533, conkeldurr: 534, tympole: 535, palpitoad: 536, seismitoad: 537, throh: 538, sawk: 539, sewaddle: 540, swadloon: 541, leavanny: 542, venipede: 543, whirlipede: 544, scolipede: 545, cottonee: 546, whimsicott: 547, petilil: 548, lilligant: 549, basculin: 550, sandile: 551, krokorok: 552, krookodile: 553, darumaka: 554, darmanitan: 555, maractus: 556, dwebble: 557, crustle: 558, scraggy: 559, scrafty: 560, sigilyph: 561, yamask: 562, cofagrigus: 563, tirtouga: 564, carracosta: 565, archen: 566, archeops: 567, trubbish: 568, garbodor: 569, zorua: 570, zoroark: 571, minccino: 572, cinccino: 573, gothita: 574, gothorita: 575, gothitelle: 576, solosis: 577, duosion: 578, reuniclus: 579, ducklett: 580, swanna: 581, vanillite: 582, vanillish: 583, vanilluxe: 584, deerling: 585, sawsbuck: 586, emolga: 587, karrablast: 588, escavalier: 589, foongus: 590, amoonguss: 591, frillish: 592, jellicent: 593, alomomola: 594, joltik: 595, galvantula: 596, ferroseed: 597, ferrothorn: 598, klink: 599, klang: 600, klinklang: 601, tynamo: 602, eelektrik: 603, eelektross: 604, elgyem: 605, beheeyem: 606, litwick: 607, lampent: 608, chandelure: 609, axew: 610, fraxure: 611, haxorus: 612, cubchoo: 613, beartic: 614, cryogonal: 615, shelmet: 616, accelgor: 617, stunfisk: 618, mienfoo: 619, mienshao: 620, druddigon: 621, golett: 622, golurk: 623, pawniard: 624, bisharp: 625, bouffalant: 626, rufflet: 627, braviary: 628, vullaby: 629, mandibuzz: 630, heatmor: 631, durant: 632, deino: 633, zweilous: 634, hydreigon: 635, larvesta: 636, volcarona: 637, cobalion: 638, terrakion: 639, virizion: 640, tornadus: 641, thundurus: 642, reshiram: 643, zekrom: 644, landorus: 645, kyurem: 646, keldeo: 647, meloetta: 648, genesect: 649,
+
 };
 
 export const EVO_MAP: { [key: string]: any } = {
@@ -52,14 +57,16 @@ export const EVO_MAP: { [key: string]: any } = {
   krabby: { next: 'kingler', level: 28 }, voltorb: { next: 'electrode', level: 30 },
   exeggcute: { next: 'exeggutor', item: 'Leaf Stone' }, cubone: { next: 'marowak', level: 28 },
   koffing: { next: 'weezing', level: 35 }, rhyhorn: { next: 'rhydon', level: 42 },
-  horsea: { next: 'seadra', level: 32 }, seaking: { next: 'goldeen', level: 33 },
+  horsea: { next: 'seadra', level: 32 }, goldeen: { next: 'seaking', level: 33 },
   staryu: { next: 'starmie', item: 'Water Stone' }, magikarp: { next: 'gyarados', level: 20 },
   eevee: [
     { next: 'vaporeon', item: 'Water Stone' },
     { next: 'jolteon', item: 'Thunder Stone' },
     { next: 'flareon', item: 'Fire Stone' },
     { next: 'espeon', level: 25 },
-    { next: 'umbreon', level: 25 }
+    { next: 'umbreon', level: 25 },
+    { next: 'leafeon', item: 'Leaf Stone' },
+    { next: 'glaceon', item: 'Water Stone' }
   ],
   omanyte: { next: 'omastar', level: 40 },
   kabuto: { next: 'kabutops', level: 40 }, dratini: { next: 'dragonair', level: 30 },
@@ -110,10 +117,16 @@ export const EVO_MAP: { [key: string]: any } = {
   lotad: { next: 'lombre', level: 14 }, lombre: { next: 'ludicolo', item: 'Water Stone' },
   seedot: { next: 'nuzleaf', level: 14 }, nuzleaf: { next: 'shiftry', item: 'Leaf Stone' },
   taillow: { next: 'swellow', level: 22 }, wingull: { next: 'pelipper', level: 25 },
-  ralts: { next: 'kirlia', level: 20 }, kirlia: { next: 'gardevoir', level: 30 },
+  ralts: { next: 'kirlia', level: 20 }, kirlia: [
+    { next: 'gardevoir', level: 30 },
+    { next: 'gallade', item: 'Moon Stone' }
+  ],
   surskit: { next: 'masquerain', level: 22 }, shroomish: { next: 'breloom', level: 23 },
   slakoth: { next: 'vigoroth', level: 18 }, vigoroth: { next: 'slaking', level: 36 },
-  nincada: { next: 'ninjask', level: 20 }, whismur: { next: 'loudred', level: 20 },
+  nincada: [
+    { next: 'ninjask', level: 20 },
+    { next: 'shedinja', level: 20 }
+  ], whismur: { next: 'loudred', level: 20 },
   loudred: { next: 'exploud', level: 40 }, makuhita: { next: 'hariyama', level: 24 },
   skitty: { next: 'delcatty', item: 'Moon Stone' }, aron: { next: 'lairon', level: 32 },
   lairon: { next: 'aggron', level: 42 }, meditite: { next: 'medicham', level: 37 },
@@ -134,19 +147,124 @@ export const EVO_MAP: { [key: string]: any } = {
   teddiursa: { next: 'ursaring', level: 30 },
   spoink: { next: 'grumpig', level: 32 },
   feebas: { next: 'milotic', level: 30 },
-  snorunt: { next: 'glalie', level: 42 },
+  snorunt: [
+    { next: 'glalie', level: 42 },
+    { next: 'froslass', item: 'Moon Stone' }
+  ],
   clamperl: [
     { next: 'huntail', item: 'Water Stone' },
     { next: 'gorebyss', item: 'Moon Stone' }
   ],
+  // Gen 4
+  turtwig: { next: 'grotle', level: 18 }, grotle: { next: 'torterra', level: 32 },
+  chimchar: { next: 'monferno', level: 14 }, monferno: { next: 'infernape', level: 36 },
+  piplup: { next: 'prinplup', level: 16 }, prinplup: { next: 'empoleon', level: 36 },
+  starly: { next: 'staravia', level: 14 }, staravia: { next: 'staraptor', level: 34 },
+  bidoof: { next: 'bibarel', level: 15 }, kricketot: { next: 'kricketune', level: 10 },
+  shinx: { next: 'luxio', level: 15 }, luxio: { next: 'luxray', level: 30 },
+  budew: { next: 'roselia', level: 12 }, cranidos: { next: 'rampardos', level: 30 },
+  shieldon: { next: 'bastiodon', level: 30 },
+  burmy: [
+    { next: 'wormadam', level: 20 },
+    { next: 'mothim', level: 20 }
+  ],
+  combee: { next: 'vespiquen', level: 21 }, buizel: { next: 'floatzel', level: 26 },
+  cherubi: { next: 'cherrim', level: 25 }, shellos: { next: 'gastrodon', level: 30 },
+  drifloon: { next: 'drifblim', level: 28 }, buneary: { next: 'lopunny', level: 24 },
+  glameow: { next: 'purugly', level: 38 }, stunky: { next: 'skuntank', level: 34 },
+  bronzor: { next: 'bronzong', level: 33 }, gible: { next: 'gabite', level: 24 },
+  gabite: { next: 'garchomp', level: 48 }, riolu: { next: 'lucario', level: 25 },
+  hippopotas: { next: 'hippowdon', level: 34 }, skorupi: { next: 'drapion', level: 40 },
+  croagunk: { next: 'toxicroak', level: 37 }, finneon: { next: 'lumineon', level: 31 },
+  snover: { next: 'abomasnow', level: 40 },
+  // Gen 4 cross-gen evolutions
+  roselia: { next: 'roserade', item: 'Sun Stone' },
+  aipom: { next: 'ambipom', level: 32 },
+  misdreavus: { next: 'mismagius', item: 'Moon Stone' },
+  murkrow: { next: 'honchkrow', item: 'Moon Stone' },
+  chingling: { next: 'chimecho', level: 15 },
+  bonsly: { next: 'sudowoodo', level: 15 },
+  mime_jr: { next: 'mr_mime', level: 15 },
+  happiny: { next: 'chansey', level: 15 },
+  munchlax: { next: 'snorlax', level: 20 },
+  mantyke: { next: 'mantine', level: 20 },
+  sneasel: { next: 'weavile', level: 30 },
+  magneton: { next: 'magnezone', level: 40 },
+  lickitung: { next: 'lickilicky', level: 32 },
+  rhydon: { next: 'rhyperior', item: 'Metal Coat' },
+  tangela: { next: 'tangrowth', level: 32 },
+  electabuzz: { next: 'electivire', level: 40 },
+  magmar: { next: 'magmortar', level: 40 },
+  togetic: { next: 'togekiss', item: 'Sun Stone' },
+  yanma: { next: 'yanmega', level: 33 },
+  gligar: { next: 'gliscor', level: 40 },
+  piloswine: { next: 'mamoswine', level: 40 },
+  porygon: { next: 'porygon2', item: 'Metal Coat' },
+  porygon2: { next: 'porygon_z', level: 40 },
+  nosepass: { next: 'probopass', level: 40 },
+  dusclops: { next: 'dusknoir', level: 40 },
+
+  // Gen 5
+  snivy: { next: 'servine', level: 17 }, servine: { next: 'serperior', level: 36 },
+  tepig: { next: 'pignite', level: 17 }, pignite: { next: 'emboar', level: 36 },
+  oshawott: { next: 'dewott', level: 17 }, dewott: { next: 'samurott', level: 36 },
+  patrat: { next: 'watchog', level: 20 },
+  lillipup: { next: 'herdier', level: 16 }, herdier: { next: 'stoutland', level: 32 },
+  purrloin: { next: 'liepard', level: 20 },
+  pansage: { next: 'simisage', item: 'Leaf Stone' },
+  pansear: { next: 'simisear', item: 'Fire Stone' },
+  panpour: { next: 'simipour', item: 'Water Stone' },
+  munna: { next: 'musharna', item: 'Moon Stone' },
+  pidove: { next: 'tranquill', level: 21 }, tranquill: { next: 'unfezant', level: 32 },
+  blitzle: { next: 'zebstrika', level: 27 },
+  roggenrola: { next: 'boldore', level: 25 }, boldore: { next: 'gigalith', level: 40 },
+  woobat: { next: 'swoobat', level: 20 },
+  drilbur: { next: 'excadrill', level: 31 },
+  timburr: { next: 'gurdurr', level: 25 }, gurdurr: { next: 'conkeldurr', level: 40 },
+  tympole: { next: 'palpitoad', level: 25 }, palpitoad: { next: 'seismitoad', level: 36 },
+  sewaddle: { next: 'swadloon', level: 20 }, swadloon: { next: 'leavanny', level: 30 },
+  venipede: { next: 'whirlipede', level: 22 }, whirlipede: { next: 'scolipede', level: 30 },
+  cottonee: { next: 'whimsicott', item: 'Sun Stone' },
+  petilil: { next: 'lilligant', item: 'Sun Stone' },
+  sandile: { next: 'krokorok', level: 29 }, krokorok: { next: 'krookodile', level: 40 },
+  darumaka: { next: 'darmanitan', level: 35 }, dwebble: { next: 'crustle', level: 34 },
+  scraggy: { next: 'scrafty', level: 39 }, yamask: { next: 'cofagrigus', level: 34 },
+  tirtouga: { next: 'carracosta', level: 37 }, archen: { next: 'archeops', level: 37 },
+  trubbish: { next: 'garbodor', level: 36 }, zorua: { next: 'zoroark', level: 30 },
+  minccino: { next: 'cinccino', item: 'Sun Stone' },
+  gothita: { next: 'gothorita', level: 32 }, gothorita: { next: 'gothitelle', level: 41 },
+  solosis: { next: 'duosion', level: 32 }, duosion: { next: 'reuniclus', level: 41 },
+  ducklett: { next: 'swanna', level: 35 },
+  vanillite: { next: 'vanillish', level: 35 }, vanillish: { next: 'vanilluxe', level: 47 },
+  deerling: { next: 'sawsbuck', level: 34 }, foongus: { next: 'amoonguss', level: 39 },
+  frillish: { next: 'jellicent', level: 40 }, joltik: { next: 'galvantula', level: 36 },
+  ferroseed: { next: 'ferrothorn', level: 40 }, klink: { next: 'klang', level: 38 },
+  klang: { next: 'klinklang', level: 49 }, tynamo: { next: 'eelektrik', level: 39 },
+  eelektrik: { next: 'eelektross', item: 'Thunder Stone' },
+  elgyem: { next: 'beheeyem', level: 42 },
+  litwick: { next: 'lampent', level: 41 }, lampent: { next: 'chandelure', item: 'Moon Stone' },
+  axew: { next: 'fraxure', level: 38 }, fraxure: { next: 'haxorus', level: 48 },
+  cubchoo: { next: 'beartic', level: 37 }, shelmet: { next: 'accelgor', level: 40 },
+  mienfoo: { next: 'mienshao', level: 50 }, golett: { next: 'golurk', level: 43 },
+  pawniard: { next: 'bisharp', level: 52 }, rufflet: { next: 'braviary', level: 54 },
+  vullaby: { next: 'mandibuzz', level: 54 }, deino: { next: 'zweilous', level: 50 },
+  zweilous: { next: 'hydreigon', level: 64 }, larvesta: { next: 'volcarona', level: 59 },
 };
 
-export const LEGENDARY = ['articuno', 'zapdos', 'moltres', 'mewtwo', 'mew', 'raikou', 'entei', 'suicune', 'lugia', 'ho_oh', 'celebi', 'regirock', 'regice', 'registeel', 'latias', 'latios', 'kyogre', 'groudon', 'rayquaza', 'jirachi', 'deoxys'];
+export const LEGENDARY = [
+    'articuno', 'zapdos', 'moltres', 'mewtwo', 'mew',
+    'raikou', 'entei', 'suicune', 'lugia', 'ho_oh', 'celebi',
+    'regirock', 'regice', 'registeel', 'latias', 'latios', 'kyogre', 'groudon', 'rayquaza', 'jirachi', 'deoxys',
+    'uxie', 'mesprit', 'azelf', 'dialga', 'palkia', 'heatran', 'regigigas', 'giratina', 'cresselia', 'phione', 'manaphy', 'darkrai', 'shaymin', 'arceus',
+    'victini', 'cobalion', 'terrakion', 'virizion', 'tornadus', 'thundurus', 'reshiram', 'zekrom', 'landorus', 'kyurem', 'keldeo', 'meloetta', 'genesect'
+];
 
 export const ALL_POKEMON_LIST = Object.keys(POKE_IDS).sort((a, b) => POKE_IDS[a] - POKE_IDS[b]);
 export const KANTO = ALL_POKEMON_LIST.filter(p => POKE_IDS[p] <= 151);
 export const JOHTO = ALL_POKEMON_LIST.filter(p => POKE_IDS[p] > 151 && POKE_IDS[p] <= 251);
 export const HOENN = ALL_POKEMON_LIST.filter(p => POKE_IDS[p] > 251 && POKE_IDS[p] <= 386);
+export const SINNOH = ALL_POKEMON_LIST.filter(p => POKE_IDS[p] > 386 && POKE_IDS[p] <= 493);
+export const UNOVA = ALL_POKEMON_LIST.filter(p => POKE_IDS[p] > 493 && POKE_IDS[p] <= 649);
 
 export const EVOLVED_NAMES = new Set(
   Object.entries(EVO_MAP).flatMap(([prev, evo]) => {
@@ -160,7 +278,9 @@ export const EVOLVED_NAMES = new Set(
 export const KANTO_BASE = KANTO.filter(p => !EVOLVED_NAMES.has(p) && !LEGENDARY.includes(p));
 export const JOHTO_BASE = JOHTO.filter(p => !EVOLVED_NAMES.has(p) && !LEGENDARY.includes(p));
 export const HOENN_BASE = HOENN.filter(p => !EVOLVED_NAMES.has(p) && !LEGENDARY.includes(p));
-export const ALL_BASE = [...KANTO_BASE, ...JOHTO_BASE, ...HOENN_BASE];
+export const SINNOH_BASE = SINNOH.filter(p => !EVOLVED_NAMES.has(p) && !LEGENDARY.includes(p));
+export const UNOVA_BASE = UNOVA.filter(p => !EVOLVED_NAMES.has(p) && !LEGENDARY.includes(p));
+export const ALL_BASE = [...KANTO_BASE, ...JOHTO_BASE, ...HOENN_BASE, ...SINNOH_BASE, ...UNOVA_BASE];
 
 export const getIconUri = (name: string) => {
   if (!name) return "";
